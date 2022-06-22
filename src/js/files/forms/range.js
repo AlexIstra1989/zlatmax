1d4389b54d531bf2fa87dcc0972b562c73882de3
AlexIstra1989 <alex888fcsm@gmail.com>
2022-06-22 23:56:42 +0300

    added catalog content

--- src/js/files/forms/range.js
+++ src/js/files/forms/range.js
@@ -8,35 +8,53 @@ import * as noUiSlider from 'nouislider';
 // import 'nouislider/dist/nouislider.css';
 
 export function rangeInit() {
-	const priceSlider = document.querySelector('#range');
-	if (priceSlider) {
-		let textFrom = priceSlider.getAttribute('data-from');
-		let textTo = priceSlider.getAttribute('data-to');
-		noUiSlider.create(priceSlider, {
-			start: 0, // [0,200000]
-			connect: [true, false],
-			range: {
-				'min': [0],
-				'max': [200000]
-			}
-		});
-		/*
-		const priceStart = document.getElementById('price-start');
-		const priceEnd = document.getElementById('price-end');
-		priceStart.addEventListener('change', setPriceValues);
-		priceEnd.addEventListener('change', setPriceValues);
-		*/
-		function setPriceValues() {
-			let priceStartValue;
-			let priceEndValue;
-			if (priceStart.value != '') {
-				priceStartValue = priceStart.value;
-			}
-			if (priceEnd.value != '') {
-				priceEndValue = priceEnd.value;
-			}
-			priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
-		}
-	}
+
+ const rangeItems = document.querySelectorAll('[data-range]');
+ if (rangeItems.length) {
+  rangeItems.forEach(rangeItem => {
+   const fromValue = rangeItem.querySelector('[data-range-from]');
+   const toValue = rangeItem.querySelector('[data-range-to]');
+   const item = rangeItem.querySelector('[data-range-item]');
+   noUiSlider.create(priceSlider, {
+    start: [fromValue.value, toValue.value], // [0,200000]
+    connect: [true, true],
+    range: {
+     'min': [fromValue.dataset.rangeRom],
+     'max': [toValue.rangeT0]
+    }
+   });
+  });
+ }
+
+	// const priceSlider = document.querySelector('#range');
+	// if (priceSlider) {
+	// 	let textFrom = priceSlider.getAttribute('data-from');
+	// 	let textTo = priceSlider.getAttribute('data-to');
+	// 	noUiSlider.create(priceSlider, {
+	// 		start: 0, // [0,200000]
+	// 		connect: [true, false],
+	// 		range: {
+	// 			'min': [0],
+	// 			'max': [200000]
+	// 		}
+	// 	});
+	// 	/*
+	// 	const priceStart = document.getElementById('price-start');
+	// 	const priceEnd = document.getElementById('price-end');
+	// 	priceStart.addEventListener('change', setPriceValues);
+	// 	priceEnd.addEventListener('change', setPriceValues);
+	// 	*/
+	// 	function setPriceValues() {
+	// 		let priceStartValue;
+	// 		let priceEndValue;
+	// 		if (priceStart.value != '') {
+	// 			priceStartValue = priceStart.value;
+	// 		}
+	// 		if (priceEnd.value != '') {
+	// 			priceEndValue = priceEnd.value;
+	// 		}
+	// 		priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
+	// 	}
+	// }
 }
 rangeInit();
